cadbabfef489fc19333e6dbc120f4bd6fb132e5e
marvenmalope <marvenmalope2003@gmail.com>
2024-09-14 13:45:23 +0200

    spring initilizr

---
+++ springfirebase/src/main/java/igris/marvin/springfirebase/SpringfirebaseApplication.java
@@ -0,0 +1,15 @@
+package igris.marvin.springfirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfirebaseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringfirebaseApplication.class, args);
+		
+	}
+
+}
---
+++ springfirebase/src/test/java/igris/marvin/springfirebase/SpringfirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package igris.marvin.springfirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
